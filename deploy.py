0ea0cb631a14bb9c330b34c72133addd80421f19
Alex Pedersen <me@alexpdr.dev>
2021-05-21 15:28:33 +0200

    different operator

--- deploy.py
+++ deploy.py
@@ -37,7 +37,7 @@ def deploy(root_path: str, prefix: str, update_type: str):
             if (
                 file.name.endswith(".env")
                 or Path(file).is_dir()
-                or "drone" in Path(file)
+                or str(Path(file)).find("drone")
             ):
                 continue
             elif file.name.startswith("dist"):
